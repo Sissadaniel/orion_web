e2c9d395fef538f092119ab222be204547f6e6b2
Matias Gianesini <matias.rodgian@gmail.com>
2017-04-17 13:13:06 -0500

    WIP: Make contact forms work

---
+++ bolsa_contact.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$formcontent="From: $name \n Phone: $phone";
+$recipient = "matias.rodgian@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
